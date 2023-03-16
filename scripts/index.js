f8142c0dbfd57e4472ee56eb638eb946112e916e
Sayaka Vincent <sfv2010@hotmail.fr>
2023-03-16 23:46:20 +0100

    Add showList and listFactory

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,21 @@
 import { recipes } from "./data/recipes.js";
-import { recipesFactory } from "./recipesFactory.js";
-import { searchInput } from "./search.js";
+import { recipesFactory } from "./factories/recipesFactory.js";
+import { searchInput } from "./utils/search.js";
+import { showList } from "./utils/list.js";
+import { listFactory } from "./factories/listFactory.js";
 
 function displayData(recipes) {
     const recipeMain = document.querySelector("main");
+    const divRecipe = document.getElementById("dropDownIngredient");
     //Loop through the recipes array and create a card for each
     recipes.forEach((recipe) => {
         const recipeInfo = recipesFactory(recipe);
         const recipeCardDOM = recipeInfo.getRecipeCardDOM();
         recipeMain.appendChild(recipeCardDOM);
+
+        const recipeList = listFactory(recipe);
+        const recipeListDOM = recipeList.getRecipeListDOM();
+        divRecipe.appendChild(recipeListDOM);
     });
 }
 //Function to get and display recipe data
@@ -18,3 +25,4 @@ function init() {
 init();
 
 searchInput();
+showList();
