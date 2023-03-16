50a67b56732a13442326fb6bcac35252698501f3
Sayaka Vincent <sfv2010@hotmail.fr>
2023-03-16 10:30:15 +0100

    Add searchInput

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,20 @@
 import { recipes } from "./data/recipes.js";
 import { recipesFactory } from "./recipesFactory.js";
+import { searchInput } from "./search.js";
 
 function displayData(recipes) {
     const recipeMain = document.querySelector("main");
     //Loop through the recipes array and create a card for each
     recipes.forEach((recipe) => {
         const recipeInfo = recipesFactory(recipe);
-        //create each recipe in a Dom element
         const recipeCardDOM = recipeInfo.getRecipeCardDOM();
         recipeMain.appendChild(recipeCardDOM);
     });
 }
 //Function to get and display recipe data
 function init() {
-    console.log(recipes);
     displayData(recipes);
 }
 init();
+
+searchInput();
