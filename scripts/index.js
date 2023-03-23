6c60494b0ed800009cb9e3f7a2956a29aa62bc81
Sayaka Vincent <sfv2010@hotmail.fr>
2023-03-23 17:39:56 +0100

    Add list

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,78 @@
 import { recipes } from "./data/recipes.js";
 import { recipesFactory } from "./factories/recipesFactory.js";
-import { searchInput } from "./utils/search.js";
+//import { searchInMainBar } from "./utils/searchInMainBar.js";
 import { showList } from "./utils/list.js";
-import { listFactory } from "./factories/listFactory.js";
 
 function displayData(recipes) {
     const recipeMain = document.querySelector("main");
-    const divRecipe = document.getElementById("dropDownIngredient");
-    //Loop through the recipes array and create a card for each
+    const ulIngredient = document.querySelector(".ulIngredient");
+    const ulAppliance = document.querySelector(".ulAppliance");
+    const ulUstensile = document.querySelector(".ulUstensile");
+    let arrayIngredients = [];
+    let arrayAppliances = [];
+    let arrayUstensils = [];
+
+    //Loop through the recipes array and create a list for each
+    //list Ingredient
     recipes.forEach((recipe) => {
+        recipe.ingredients.forEach((ingredientKey) => {
+            arrayIngredients.push(ingredientKey.ingredient.toLowerCase());
+        });
+
+        // console.log(recipe.appliance);
+        // arrayAppliances.push(recipe.applience);
+        // console.log(arrayAppliances);
+
         const recipeInfo = recipesFactory(recipe);
         const recipeCardDOM = recipeInfo.getRecipeCardDOM();
         recipeMain.appendChild(recipeCardDOM);
+    });
+    for (let i in arrayIngredients) {
+        arrayIngredients[i] =
+            arrayIngredients[i].charAt(0).toUpperCase() +
+            arrayIngredients[i].slice(1);
+    }
+    let sortIngredients = [...new Set(arrayIngredients)].sort();
+    sortIngredients.forEach((ingredient) => {
+        const listRecipe = document.createElement("li");
+        listRecipe.classList.add("listRecipe");
+        listRecipe.textContent = ingredient;
+        ulIngredient.appendChild(listRecipe);
+    });
 
-        const recipeList = listFactory(recipe);
-        const recipeListDOM = recipeList.getRecipeListDOM();
-        divRecipe.appendChild(recipeListDOM);
+    //list Appliance
+    recipes.forEach((recipe) => {
+        arrayAppliances.push(recipe.appliance);
+    });
+
+    let sortAppliances = [...new Set(arrayAppliances)].sort();
+    sortAppliances.forEach((appliance) => {
+        const listRecipe = document.createElement("li");
+        listRecipe.classList.add("listRecipe");
+        listRecipe.textContent = appliance;
+        ulAppliance.appendChild(listRecipe);
+    });
+
+    //list Ustensile
+    recipes.forEach((recipe) => {
+        recipe.ustensils.forEach((ustensilKey) => {
+            arrayUstensils.push(ustensilKey);
+        });
+        // arrayUstensils.push(recipe.ustensils);
+        //console.log(recipe.ustensils);
+    });
+    for (let i in arrayUstensils) {
+        arrayUstensils[i] =
+            arrayUstensils[i].charAt(0).toUpperCase() +
+            arrayUstensils[i].slice(1);
+    }
+
+    let sortUstensiles = [...new Set(arrayUstensils)].sort();
+    sortUstensiles.forEach((ustensile) => {
+        const listRecipe = document.createElement("li");
+        listRecipe.classList.add("listRecipe");
+        listRecipe.textContent = ustensile;
+        ulUstensile.appendChild(listRecipe);
     });
 }
 //Function to get and display recipe data
@@ -24,5 +81,5 @@ function init() {
 }
 init();
 
-searchInput();
+//searchInMainBar();
 showList();
