a4731314479ef699efd434a74481dff715c8b206
Sayaka Vincent <sfv2010@hotmail.fr>
2023-03-24 11:31:53 +0100

    Code refactoring

--- scripts/utils/list.js
+++ scripts/utils/list.js
@@ -17,41 +17,28 @@ export function showList() {
     const imgUps = [imgUpIngredient, imgUpAppliance, imgUpUstensile];
 
     function openList(e) {
+        for (let button of buttons) button.style.display = "block";
+        for (let dropDown of dropDowns) dropDown.style.display = "none";
+
         if (e.target.textContent === "Ingredient") {
             buttonIngredient.style.display = "none";
-            buttonAppliance.style.display = "block";
-            buttonUstensile.style.display = "block";
             dropDownIngredient.style.display = "block";
-            dropDownAppliance.style.display = "none";
-            dropDownUstensile.style.display = "none";
         } else if (e.target.textContent === "Appareils") {
             buttonAppliance.style.display = "none";
-            buttonIngredient.style.display = "block";
-            buttonUstensile.style.display = "block";
             dropDownAppliance.style.display = "block";
-            dropDownIngredient.style.display = "none";
-            dropDownUstensile.style.display = "none";
         } else if (e.target.textContent === "Ustensiles") {
             buttonUstensile.style.display = "none";
-            buttonIngredient.style.display = "block";
-            buttonAppliance.style.display = "block";
             dropDownUstensile.style.display = "block";
-            dropDownIngredient.style.display = "none";
-            dropDownAppliance.style.display = "none";
         }
     }
-
     function closeList(e) {
-        //console.log(e.target.id);
-        for (let button of buttons) {
-            button.style.display = "block";
-        }
+        for (let button of buttons) button.style.display = "block";
         for (let dropDown of dropDowns) dropDown.style.display = "none";
     }
+
     buttons.forEach((button) =>
         button.addEventListener("click", (e) => openList(e))
     );
-
     imgUps.forEach((imgUp) =>
         imgUp.addEventListener("click", (e) => closeList(e))
     );
