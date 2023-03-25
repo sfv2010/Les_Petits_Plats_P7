76e20fd48884cb1b1b44d7dcfad85ec2f0d8a0e2
Sayaka Vincent <sfv2010@hotmail.fr>
2023-03-25 17:33:51 +0100

    Add searchByKeyword

---
+++ scripts/searchByKeyword.js
@@ -0,0 +1,26 @@
+// export function searchByKeyword() {
+//     const input = document.querySelectorAll(".searchBox");
+// 	// const ingredient = document.getElementById(".ingredient");
+// 	// const listAppliance = document.getElementById(".listAppliance");
+// 	// const listUstencile = document.getElementById(".listUstencile");
+//     //const cards = document.querySelectorAll("article");
+
+//     //Search based on input value
+//     // input.addEventListener("keyup", function (e) {
+//         // const searchInput = e.target.value;
+//         //     cards.forEach((card) => {
+//         //         if (
+//         //             card.textContent
+//         //                 .toLowerCase()
+//         //                 .includes(searchInput.toLowerCase())
+//         //         ) {
+//         //             card.classList.remove("hidden");
+//         //         } else {
+//         //             card.classList.add("hidden");
+//         //         }
+//         //     });
+//         //  if (searchInput.length === 0) {
+//         //     cards.forEach((card) => card.classList.remove("hidden"));
+//         // }
+//     });
+// }
