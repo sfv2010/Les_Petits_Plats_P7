a7271d872ff6c2be91ed8b2b61bb94b33b196295
Sayaka Vincent <sfv2010@hotmail.fr>
2023-03-27 18:59:03 +0200

    Fix noFound

--- scripts/utils/searchInMainBar.js
+++ scripts/utils/searchInMainBar.js
@@ -8,22 +8,21 @@ export function searchInMainBar() {
     input.addEventListener("keyup", function (e) {
         const searchInput = e.target.value;
         let numHidden = 0;
-        noFound.textContent = "";
+        //noFound.textContent = "";
         if (searchInput.length >= 3) {
             cards.forEach((card) => {
-                // console.log(card.classList.value);
-
                 card.textContent
                     .toLowerCase()
                     .includes(searchInput.toLowerCase())
                     ? (card.classList.remove("hidden"), numHidden++)
                     : card.classList.add("hidden");
             });
-            numHidden === 0 &&
-                ((noFound.textContent =
-                    " Aucune recette ne correspond à votre critère… vous pouvez chercher  « tarte aux pommes », « poisson », etc."),
-                noFound.classList.add("noFound"),
-                main.appendChild(noFound));
+            numHidden === 0
+                ? ((noFound.textContent =
+                      " Aucune recette ne correspond à votre critère… vous pouvez chercher  « tarte aux pommes », « poisson », etc."),
+                  noFound.classList.add("noFound"),
+                  main.appendChild(noFound))
+                : (noFound.textContent = "");
         }
         if (searchInput.length === 0) {
             cards.forEach((card) => card.classList.remove("hidden"));
