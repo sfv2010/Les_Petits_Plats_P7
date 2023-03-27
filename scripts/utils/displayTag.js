7b758d65e1f08f32a8ea8d85b4130e747416e2f2
Sayaka Vincent <sfv2010@hotmail.fr>
2023-03-27 14:49:04 +0200

    Add const cards

--- scripts/utils/displayTag.js
+++ scripts/utils/displayTag.js
@@ -2,11 +2,14 @@ import { searchByKeyword } from "./searchByKeyword.js";
 export function displayTag() {
     const listRecipes = document.querySelectorAll(".listRecipe");
     const tagsContainer = document.querySelector(".tagsContainer");
+    const cards = document.querySelectorAll("article");
 
     function toggleTag(e) {
+        //create tag
+        const searchByTag = e.target.textContent;
         const tagRecipe = document.createElement("span");
         tagRecipe.classList.add("tag");
-        tagRecipe.textContent = e.target.textContent;
+        tagRecipe.textContent = searchByTag;
         tagRecipe.tabIndex = "0";
         tagsContainer.appendChild(tagRecipe);
         if (e.target.classList.value === "listRecipe ingredients") {
@@ -18,10 +21,35 @@ export function displayTag() {
         if (e.target.classList.value === "listRecipe ustensiles") {
             tagRecipe.classList.add("ustensiles");
         }
+        e.target.classList.add("hidden");
+
+        //sort
+        console.log(e.target);
+        cards.forEach((card) => {
+            if (
+                card.textContent
+                    .toLowerCase()
+                    .includes(searchByTag.toLowerCase())
+            ) {
+                card.classList.remove("hidden");
+            } else {
+                card.classList.add("hidden");
+            }
+        });
+        // if (!e.target) {
+        //     cards.forEach((card) => card.classList.remove("hidden"));
+        // }
 
         //close tag
         function closeTag(e) {
+            console.log(e.target);
             e.target.style.display = "none";
+            cards.forEach((card) => card.classList.remove("hidden"));
+            listRecipes.forEach((listRecipe) => {
+                if (e.target.textContent === listRecipe.textContent) {
+                    listRecipe.classList.remove("hidden");
+                }
+            });
         }
         const tags = document.querySelectorAll(".tag");
         tags.forEach((tag) => {
