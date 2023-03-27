3a56ce93a0666a3cf171dbfff372c989147183ee
Sayaka Vincent <sfv2010@hotmail.fr>
2023-03-27 14:49:35 +0200

    Add noFound

--- scripts/utils/searchInMainBar.js
+++ scripts/utils/searchInMainBar.js
@@ -1,23 +1,31 @@
 export function searchInMainBar() {
     const input = document.querySelector("#search");
     const cards = document.querySelectorAll("article");
+    const main = document.querySelector("main");
+    const noFound = document.createElement("div");
 
     //Search based on input value
     input.addEventListener("keyup", function (e) {
         const searchInput = e.target.value;
+        let numHidden = 0;
+        // noFound.textContent = "";
         if (searchInput.length >= 3) {
             for (let i = 0; i < cards.length; i++) {
-                if (
-                    cards[i].textContent
-                        .toLowerCase()
-                        .includes(searchInput.toLowerCase())
-                ) {
-                    cards[i].classList.remove("hidden");
-                } else {
-                    cards[i].classList.add("hidden");
-                }
+                cards[i].textContent
+                    .toLowerCase()
+                    .includes(searchInput.toLowerCase())
+                    ? (cards[i].classList.remove("hidden"), numHidden++)
+                    : cards[i].classList.add("hidden");
+
+                numHidden === 0
+                    ? ((noFound.textContent =
+                          " Aucune recette ne correspond à votre critère… vous pouvez chercher  « tarte aux pommes », « poisson », etc."),
+                      noFound.classList.add("noFound"),
+                      main.appendChild(noFound))
+                    : (noFound.textContent = "");
             }
-        } else if (searchInput.length === 0) {
+        }
+        if (searchInput.length === 0) {
             cards.forEach((card) => card.classList.remove("hidden"));
         }
     });
