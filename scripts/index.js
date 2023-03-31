ce6fe91a90010aeaef41ea98cc9bf2db790e1f23
Sayaka Vincent <sfv2010@hotmail.fr>
2023-03-31 11:15:58 +0200

    Fix global

--- scripts/index.js
+++ scripts/index.js
@@ -1,96 +1,92 @@
 import { recipes } from "./data/recipes.js";
 import { recipesFactory } from "./factories/recipesFactory.js";
 import { searchInMainBar } from "./utils/searchInMainBar.js";
-import { showList } from "./utils/list.js";
+import { openCloseList } from "./utils/openCloseList.js";
 import { displayTag } from "./utils/displayTag.js";
 
-function displayData(recipes) {
-    const recipeMain = document.querySelector("main");
-    const ulIngredient = document.querySelector(".ulIngredient");
-    const ulAppliance = document.querySelector(".ulAppliance");
-    const ulUstensile = document.querySelector(".ulUstensile");
-
-    let arrayIngredients = [];
-    let arrayAppliances = [];
-    let arrayUstensils = [];
-
-    function capitalize(array) {
-        for (let i = 0; i < array.length; i++) {
-            array[i] = array[i].charAt(0).toUpperCase() + array[i].slice(1);
-        }
+//Function to capitalize first letter
+function capitalize(array) {
+    for (let i = 0; i < array.length; i++) {
+        array[i] = array[i].charAt(0).toUpperCase() + array[i].slice(1);
     }
-
-    //Loop through the recipes array and create a list for each
-    //list Ingredient
+}
+//display recipes cards
+export function displayMainData(recipes) {
+    const main = document.querySelector("main");
     recipes.forEach((recipe) => {
-        recipe.ingredients.forEach((ingredientKey) => {
-            arrayIngredients.push(
-                ingredientKey.ingredient.toLowerCase().replace(/s\b/, "")
-            );
-        });
-
-        // console.log(recipe.appliance);
-        // arrayAppliances.push(recipe.applience);
-        // console.log(arrayAppliances);
-
         const recipeInfo = recipesFactory(recipe);
         const recipeCardDOM = recipeInfo.getRecipeCardDOM();
-        recipeMain.appendChild(recipeCardDOM);
+        main.appendChild(recipeCardDOM);
+    });
+}
+//Loop through the recipes array and create a list for each
+//list Ingredient
+export function displayIngredientData(recipes) {
+    const ulIngredient = document.querySelector(".ulIngredient");
+    let arrayIngredients = [];
+    recipes.forEach((recipe) => {
+        recipe.ingredients.forEach((ingredientKey) => {
+            arrayIngredients.push(ingredientKey.ingredient.toLowerCase());
+        });
     });
     capitalize(arrayIngredients);
     let sortIngredients = [...new Set(arrayIngredients)].sort();
-    sortIngredients.forEach((ingredient, index) => {
+    console.log(sortIngredients);
+    sortIngredients.forEach((ingredient) => {
         const listRecipe = document.createElement("li");
         listRecipe.classList.add("listRecipe");
         listRecipe.classList.add("ingredients");
-        listRecipe.id = `ingredient${index}`;
         listRecipe.textContent = ingredient;
         listRecipe.tabIndex = "0";
         ulIngredient.appendChild(listRecipe);
     });
-
-    //list Appliance
+    displayTag();
+}
+//list Appliance
+export function displayApplianceData(recipes) {
+    const ulAppliance = document.querySelector(".ulAppliance");
+    let arrayAppliances = [];
     recipes.forEach((recipe) => {
         arrayAppliances.push(recipe.appliance);
     });
-
     let sortAppliances = [...new Set(arrayAppliances)].sort();
-    sortAppliances.forEach((appliance, index) => {
+    sortAppliances.forEach((appliance) => {
         const listRecipe = document.createElement("li");
         listRecipe.classList.add("listRecipe");
         listRecipe.classList.add("appliances");
-        listRecipe.id = `appliance${index}`;
         listRecipe.textContent = appliance;
         listRecipe.tabIndex = "0";
         ulAppliance.appendChild(listRecipe);
     });
-
-    //list Ustensile
+    displayTag();
+}
+export function displayUstensileeData(recipes) {
+    const ulUstensile = document.querySelector(".ulUstensile");
+    let arrayUstensils = [];
     recipes.forEach((recipe) => {
         recipe.ustensils.forEach((ustensilKey) => {
             arrayUstensils.push(ustensilKey.replace(/[^a-z]/gi, ""));
         });
     });
+    //list Ustensile
     capitalize(arrayUstensils);
-
     let sortUstensiles = [...new Set(arrayUstensils)].sort();
-    sortUstensiles.forEach((ustensile, index) => {
+    sortUstensiles.forEach((ustensile) => {
         const listRecipe = document.createElement("li");
         listRecipe.classList.add("listRecipe");
         listRecipe.classList.add("ustensiles");
-        listRecipe.id = `ustensile${index}`;
         listRecipe.textContent = ustensile;
         listRecipe.tabIndex = "0";
         ulUstensile.appendChild(listRecipe);
     });
+    displayTag();
 }
+
 //Function to get and display recipe data
-function init() {
-    displayData(recipes);
-}
-init();
+displayMainData(recipes);
+displayIngredientData(recipes);
+displayApplianceData(recipes);
+displayUstensileeData(recipes);
 
-searchInMainBar();
-showList();
-
-displayTag();
+searchInMainBar(recipes);
+openCloseList();
