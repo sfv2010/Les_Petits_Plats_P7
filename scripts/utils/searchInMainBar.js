3a24dc071be3f369bfd8e27352621a7325c60d89
Sayaka Vincent <sfv2010@hotmail.fr>
2023-03-31 11:19:32 +0200

    Fix global

--- scripts/utils/searchInMainBar.js
+++ scripts/utils/searchInMainBar.js
@@ -1,31 +1,70 @@
-export function searchInMainBar() {
+import {
+    displayMainData,
+    displayIngredientData,
+    displayApplianceData,
+    displayUstensileeData,
+} from "../index.js";
+export function searchInMainBar(recipes) {
     const input = document.querySelector("#search");
-    const cards = document.querySelectorAll("article");
     const main = document.querySelector("main");
     const noFound = document.createElement("div");
+    const ulIngredient = document.querySelector(".ulIngredient");
+    const ulAppliance = document.querySelector(".ulAppliance");
+    const ulUstensile = document.querySelector(".ulUstensile");
 
     //Search based on input value
     input.addEventListener("keyup", function (e) {
-        const searchInput = e.target.value;
-        let numHidden = 0;
+        const searchInput = e.target.value.toLowerCase();
+        main.textContent = "";
+        ulIngredient.textContent = "";
+        ulAppliance.textContent = "";
+        ulUstensile.textContent = "";
+        let searchArray = [];
         //noFound.textContent = "";
+
         if (searchInput.length >= 3) {
-            cards.forEach((card) => {
-                card.textContent
+            recipes.forEach((recipe) => {
+                (recipe.name
                     .toLowerCase()
-                    .includes(searchInput.toLowerCase())
-                    ? (card.classList.remove("hidden"), numHidden++)
-                    : card.classList.add("hidden");
+                    .includes(searchInput.toLowerCase()) ||
+                    recipe.ingredients.forEach((ingredients) => {
+                        ingredients.ingredient
+                            .toLowerCase()
+                            .includes(searchInput.toLowerCase());
+                    }) ||
+                    recipe.description
+                        .toLowerCase()
+                        .includes(searchInput.toLowerCase())) &&
+                    searchArray.push(recipe);
             });
-            numHidden === 0
-                ? ((noFound.textContent =
-                      " Aucune recette ne correspond à votre critère… vous pouvez chercher  « tarte aux pommes », « poisson », etc."),
-                  noFound.classList.add("noFound"),
-                  main.appendChild(noFound))
-                : (noFound.textContent = "");
+            if (searchArray.length === 0) {
+                noFound.textContent =
+                    " Aucune recette ne correspond à votre critère… vous pouvez chercher  « tarte aux pommes », « poisson », etc.";
+                noFound.classList.add("noFound");
+                main.appendChild(noFound);
+            } else {
+                noFound.textContent = "";
+                displayMainData(searchArray);
+                displayIngredientData(searchArray);
+                displayApplianceData(searchArray);
+                displayUstensileeData(searchArray);
+            }
+        } else {
+            displayMainData(recipes);
+            displayIngredientData(recipes);
+            displayApplianceData(recipes);
+            displayUstensileeData(recipes);
         }
-        if (searchInput.length === 0) {
-            cards.forEach((card) => card.classList.remove("hidden"));
-        }
+    });
+    //Initialize the screen when pressing the cross button of input
+    input.addEventListener("search", function (e) {
+        main.textContent = "";
+        ulIngredient.textContent = "";
+        ulAppliance.textContent = "";
+        ulUstensile.textContent = "";
+        displayMainData(recipes);
+        displayIngredientData(recipes);
+        displayApplianceData(recipes);
+        displayUstensileeData(recipes);
     });
 }
