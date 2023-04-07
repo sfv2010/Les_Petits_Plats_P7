8e26cfcbb1c8ac7060e150fa56aa6fa8230fd86b
Sayaka Vincent <sfv2010@hotmail.fr>
2023-04-08 00:02:05 +0200

    Add mainSearch

---
+++ scripts/utils/mainSearch.js
@@ -0,0 +1,63 @@
+export function mainSearch(recipes) {
+    const input = document.querySelector("#search");
+    const main = document.querySelector("main");
+    const noFound = document.createElement("div");
+    const ulIngredient = document.querySelector(".ulIngredient");
+    const ulAppliance = document.querySelector(".ulAppliance");
+    const ulUstensile = document.querySelector(".ulUstensile");
+
+    //Search based on input value
+    input.addEventListener("keyup", function (e) {
+        const searchInput = e.target.value.toLowerCase();
+        // main.textContent = "";
+        // ulIngredient.textContent = "";
+        // ulAppliance.textContent = "";
+        // ulUstensile.textContent = "";
+        let searchArray = [];
+        //noFound.textContent = "";
+
+        if (searchInput.length >= 3) {
+            recipes.forEach((recipe) => {
+                if (
+                    recipe.name.toLowerCase().includes(searchInput) ||
+                    recipe.ingredients.find((ingredients) => {
+                        return ingredients.ingredient
+                            .toLowerCase()
+                            .includes(searchInput);
+                    }) ||
+                    recipe.description.toLowerCase().includes(searchInput) ||
+                    recipe.appliance.toLowerCase().includes(searchInput) ||
+                    recipe.ustensils.find((ustensil) => {
+                        return ustensil.toLowerCase().includes(searchInput);
+                    })
+                )
+                    return searchArray.push(recipe);
+            });
+            if (searchArray.length === 0) {
+                noFound.textContent =
+                    " Aucune recette ne correspond à votre critère… vous pouvez chercher  « tarte aux pommes », « poisson », etc.";
+                noFound.classList.add("noFound");
+                main.appendChild(noFound);
+            } else {
+                noFound.textContent = "";
+            }
+        }
+        // else {
+        //     displayMainData(recipes);
+        //     displayIngredientData(recipes);
+        //     displayApplianceData(recipes);
+        //     displayUstensileData(recipes);
+        // }
+    });
+    //Initialize the screen when pressing the cross button of input
+    input.addEventListener("search", function () {
+        main.textContent = "";
+        ulIngredient.textContent = "";
+        ulAppliance.textContent = "";
+        ulUstensile.textContent = "";
+        // displayMainData(recipes);
+        // displayIngredientData(recipes);
+        // displayApplianceData(recipes);
+        // displayUstensileData(recipes);
+    });
+}
