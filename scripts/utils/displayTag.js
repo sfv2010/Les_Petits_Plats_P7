daf279c359ae8fbf7480f784175ab2d036adce14
Sayaka Vincent <sfv2010@hotmail.fr>
2023-04-07 10:16:30 +0200

    Fix global

--- scripts/utils/displayTag.js
+++ scripts/utils/displayTag.js
@@ -1,61 +1,148 @@
-import { searchByKeyword } from "./searchByKeyword.js";
-export function displayTag() {
-    const listRecipes = document.querySelectorAll(".listRecipe");
-    const tagsContainer = document.querySelector(".tagsContainer");
-    const cards = document.querySelectorAll("article");
+import {
+    displayMainData,
+    displayIngredientData,
+    displayApplianceData,
+    displayUstensileData,
+} from "../index.js";
+import { searchByInputKeyword } from "./searchByInputKeyword.js";
+import { recipes } from "../data/recipes.js";
 
+export function displayTag(recipesData, type) {
+    // export function displayTag(recipes,type) {
+    const originalRecipeData = recipes;
+    const listRecipes = document.querySelectorAll(`.listRecipe.${type}`);
+    //console.log(listRecipes);
+    const tagsContainer = document.querySelector(".tagsContainer");
+
+    //Functions to show(create), close, Sort tags
     function toggleTag(e) {
         //create tag
         const searchByTag = e.target.textContent;
-        const tagRecipe = document.createElement("span");
-        tagRecipe.classList.add("tag");
-        tagRecipe.textContent = searchByTag;
-        tagRecipe.tabIndex = "0";
-        tagsContainer.appendChild(tagRecipe);
-        if (e.target.classList.value === "listRecipe ingredients") {
-            tagRecipe.classList.add("ingredients");
+        function createTag(object) {
+            const tagRecipe = document.createElement("span");
+            tagRecipe.classList.add("tag");
+            tagRecipe.textContent = searchByTag;
+            tagRecipe.tabIndex = "0";
+            tagRecipe.classList.add("target");
+            tagRecipe.classList.add(object);
+            tagsContainer.appendChild(tagRecipe);
+            return tagRecipe;
         }
-        if (e.target.classList.value === "listRecipe appliances") {
-            tagRecipe.classList.add("appliances");
-        }
-        if (e.target.classList.value === "listRecipe ustensiles") {
-            tagRecipe.classList.add("ustensiles");
-        }
-        e.target.classList.add("hidden");
 
-        //sort
-        console.log(e.target);
-        cards.forEach((card) => {
+        if (e.target.classList.value === "listRecipe ingredients") {
+            createTag("ingredients");
+        } else if (e.target.classList.value === "listRecipe appliances") {
+            createTag("appliances");
+        } else if (e.target.classList.value === "listRecipe ustensiles") {
+            createTag("ustensiles");
+        }
+
+        console.log(e.target.classList);
+
+        //Sort the corresponding recipe when selecting from the list
+        const noFound = document.createElement("div");
+        const main = document.querySelector("main");
+        const ulIngredient = document.querySelector(".ulIngredient");
+        const ulAppliance = document.querySelector(".ulAppliance");
+        const ulUstensile = document.querySelector(".ulUstensile");
+
+        //Put all the selected tags into an array
+        const targets = document.querySelectorAll(".target");
+        let getTargetTag = [];
+
+        targets.forEach((target) => {
+            getTargetTag.push(target.textContent.toLowerCase());
+        });
+        console.log(getTargetTag);
+        const itemArray = [];
+        console.log(searchByTag);
+        console.log(itemArray);
+        recipesData.forEach((recipe) => {
             if (
-                card.textContent
+                recipe.name.toLowerCase().includes(searchByTag.toLowerCase()) ||
+                recipe.ingredients.find((ingredients) => {
+                    return ingredients.ingredient
+                        .toLowerCase()
+                        .includes(searchByTag.toLowerCase());
+                }) ||
+                recipe.description
                     .toLowerCase()
                     .includes(searchByTag.toLowerCase())
-            ) {
-                card.classList.remove("hidden");
-            } else {
-                card.classList.add("hidden");
-            }
+            )
+                itemArray.push(recipe);
         });
-        // if (!e.target) {
-        //     cards.forEach((card) => card.classList.remove("hidden"));
-        // }
+        if (itemArray.length == 0) {
+            displayMainData(recipesData);
+            displayIngredientData(recipesData);
+            displayApplianceData(recipesData);
+            displayUstensileData(recipesData);
 
+            noFound.textContent =
+                " Aucune recette ne correspond à votre critère… vous pouvez chercher  « tarte aux pommes », « poisson », etc.";
+            noFound.classList.add("noFound");
+            main.appendChild(noFound);
+        } else {
+            noFound.textContent = "";
+            main.textContent = "";
+            ulIngredient.textContent = "";
+            ulAppliance.textContent = "";
+            ulUstensile.textContent = "";
+            displayMainData(itemArray);
+            displayIngredientData(itemArray);
+            displayApplianceData(itemArray);
+            displayUstensileData(itemArray);
+        }
+        console.log(itemArray);
+        // console.log(itemArray.length);
         //close tag
         function closeTag(e) {
-            console.log(e.target);
-            e.target.style.display = "none";
-            cards.forEach((card) => card.classList.remove("hidden"));
-            listRecipes.forEach((listRecipe) => {
-                if (e.target.textContent === listRecipe.textContent) {
-                    listRecipe.classList.remove("hidden");
-                }
-            });
+            //e.stopPropagation();
+            const targetToClose = e.target;
+            console.log(targetToClose);
+
+            e.target.remove();
+            displayMainData(recipesData);
+            displayIngredientData(recipesData);
+            displayApplianceData(recipesData);
+            displayUstensileData(recipesData);
+
+            // const targets = document.querySelectorAll(".target");
+            // let getTargetTag = [];
+
+            // targets.forEach((target) => {
+            //     console.log(target.textContent.toLowerCase());
+            //     getTargetTag.push(target.textContent.toLowerCase());
+            // });
+            // console.log(getTargetTag);
+
+            //Remove targetToClose from array
+            let idx = getTargetTag.indexOf(
+                targetToClose.textContent.toLowerCase()
+            );
+            console.log(targetToClose.textContent.toLowerCase());
+            console.log(getTargetTag);
+            console.log(idx);
+            if (idx >= 0) {
+                getTargetTag.splice(idx, 1);
+                console.log(idx);
+            }
+            console.log(getTargetTag);
+
+            if (getTargetTag.length === 0) {
+                main.textContent = "";
+                ulIngredient.textContent = "";
+                ulAppliance.textContent = "";
+                ulUstensile.textContent = "";
+                displayMainData(recipesData);
+                displayIngredientData(originalRecipeData);
+                displayApplianceData(originalRecipeData);
+                displayUstensileData(originalRecipeData);
+                // getTargetTag.splice(0);
+            }
         }
         const tags = document.querySelectorAll(".tag");
         tags.forEach((tag) => {
             tag.addEventListener("click", closeTag);
-        });
-        tags.forEach((tag) => {
             tag.addEventListener("keydown", (e) => {
                 if (e.key === "Escape" || e.key === "Enter") {
                     closeTag(e);
@@ -66,13 +153,11 @@ export function displayTag() {
 
     listRecipes.forEach((listRecipe) => {
         listRecipe.addEventListener("click", toggleTag);
-    });
-    listRecipes.forEach((listRecipe) => {
         listRecipe.addEventListener("keydown", (e) => {
             if (e.key === "Enter") {
                 toggleTag(e);
             }
         });
     });
-    searchByKeyword();
+    searchByInputKeyword();
 }
