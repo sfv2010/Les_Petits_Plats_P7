6f8fb64657e618aa51819264e64a7f2ccdaf8022
Sayaka Vincent <sfv2010@hotmail.fr>
2023-04-28 11:44:10 +0200

    Refactoring

--- scripts/utils/openCloseList.js
+++ scripts/utils/openCloseList.js
@@ -1,53 +1,43 @@
 //Open/close list tag button
 export function openCloseList() {
-    const buttonIngredient = document.querySelector(".buttonIngredient");
-    const buttonAppliance = document.querySelector(".buttonAppliance");
-    const buttonUstensile = document.querySelector(".buttonUstensile");
-    const imgUpIngredient = document.getElementById("imgUpIngredient");
-    const imgUpAppliance = document.getElementById("imgUpAppliance");
-    const imgUpUstensile = document.getElementById("imgUpUstensile");
-    const dropDownIngredient = document.getElementById("dropDownIngredient");
-    const dropDownAppliance = document.getElementById("dropDownAppliance");
-    const dropDownUstensile = document.getElementById("dropDownUstensile");
-    const buttons = [buttonIngredient, buttonAppliance, buttonUstensile];
-    const dropDowns = [
-        dropDownIngredient,
-        dropDownAppliance,
-        dropDownUstensile,
-    ];
-    const imgUps = [imgUpIngredient, imgUpAppliance, imgUpUstensile];
-
+    const buttons = Array.from(
+        document.querySelectorAll(
+            ".buttonIngredient, .buttonAppliance, .buttonUstensile"
+        )
+    );
+    const dropDowns = Array.from(
+        document.querySelectorAll(
+            "#dropDownIngredient, #dropDownAppliance, #dropDownUstensile"
+        )
+    );
+    const imgUps = Array.from(
+        document.querySelectorAll(
+            "#imgUpIngredient, #imgUpAppliance, #imgUpUstensile"
+        )
+    );
     function openList(e) {
-        const eTextContent = e.target.textContent;
-        for (let button of buttons) button.style.display = "block";
-        for (let dropDown of dropDowns) dropDown.style.display = "none";
+        const targetButton = e.target;
+        const buttonIndex = buttons.indexOf(e.target);
+        const targetDropDown = dropDowns[buttonIndex];
 
-        if (eTextContent === "Ingredients") {
-            buttonIngredient.style.display = "none";
-            dropDownIngredient.style.display = "block";
-        } else if (eTextContent === "Appareils") {
-            buttonAppliance.style.display = "none";
-            dropDownAppliance.style.display = "block";
-        } else if (eTextContent === "Ustensiles") {
-            buttonUstensile.style.display = "none";
-            dropDownUstensile.style.display = "block";
-        }
+        buttons.forEach((button) => (button.style.display = "block"));
+        dropDowns.forEach((dropDown) => (dropDown.style.display = "none"));
+        //if selecting one of the buttons
+        targetButton.style.display = "none";
+        targetDropDown.style.display = "block";
     }
+
     function closeList() {
-        for (let button of buttons) button.style.display = "block";
-        for (let dropDown of dropDowns) dropDown.style.display = "none";
+        buttons.forEach((button) => (button.style.display = "block"));
+        dropDowns.forEach((dropDown) => (dropDown.style.display = "none"));
     }
 
-    buttons.forEach((button) =>
-        button.addEventListener("click", (e) => openList(e))
-    );
-    imgUps.forEach((imgUp) =>
-        imgUp.addEventListener("click", () => closeList())
-    );
+    buttons.forEach((button) => button.addEventListener("click", openList));
     imgUps.forEach((imgUp) => {
+        imgUp.addEventListener("click", closeList);
         imgUp.addEventListener("keydown", (e) => {
             if (e.key === "Escape" || e.key === "Enter") {
-                closeList(e);
+                closeList();
             }
         });
     });
